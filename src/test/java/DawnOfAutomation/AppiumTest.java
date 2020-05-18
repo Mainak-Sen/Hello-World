2c59da85b0724f664058296dda82208fc9c61adc
Mainak_Sen <mbornfree93@gmail.com>
2020-05-18 20:46:52 +0530

    Changes added in developed branch

--- src/test/java/DawnOfAutomation/AppiumTest.java
+++ src/test/java/DawnOfAutomation/AppiumTest.java
@@ -17,5 +17,18 @@ public class AppiumTest {
 		
 		System.out.println("This is Appium Test number 2");
 	}
-
+	
+	@Test
+	public void AppiumTest3()
+	{
+		
+		System.out.println("This is Appium Test number 3");
+	}
+	
+	@Test
+	public void AppiumTest4()
+	{
+		
+		System.out.println("This is Appium Test number 4");
+	}
 }
--- src/test/java/DawnOfAutomation/SeleniumTest.java
+++ src/test/java/DawnOfAutomation/SeleniumTest.java
@@ -18,4 +18,18 @@ public void SeleniumTest2()
 	System.out.println("This is Selenium Test number 2");
 }
 
+@Test
+public void SeleniumTest3()
+{
+	
+	System.out.println("This is Selenium Test number 3");
+}
+
+@Test
+public void SeleniumTest4()
+{
+	
+	System.out.println("This is Selenium Test number 4");
+}
+
 }
